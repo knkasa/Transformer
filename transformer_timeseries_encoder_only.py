272dd4046558e941f41e954683efe0e90d0a0005
Ken Nakatsukasa <knkasa@gmail.com>
2024-10-31 15:55:57 +0900

    Update transformer_timeseries_encoder_only.py

--- transformer_timeseries_encoder_only.py
+++ transformer_timeseries_encoder_only.py
@@ -142,7 +142,7 @@ class transformer_model(tf.keras.Model):
     def call(self, inputs, training):
         
         x = inputs
-        x = tf.keras.layers.Add()([x, self.pos_encoding]) 
+        x = x + self.pos_encoding   #tf.keras.layers.Add()([x, self.pos_encoding]) 
         
         # Note that after the transformer_layer, the input dimension stays the same.  
         for _ in range(self.num_transformer):  # stack number of transformer layers.
